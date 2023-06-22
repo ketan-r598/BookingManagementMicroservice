4cf09f5d7663ba9769e821226683efec3ed61696
Ketan R <ketan_r598@outlook.com>
2023-06-22 12:45:26 +0530

    updated endpoints

--- src/main/java/com/project/controller/BookingController.java
+++ src/main/java/com/project/controller/BookingController.java
@@ -5,24 +5,26 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.model.Booking;
 import com.project.service.BookingService;
 
 @RestController
+@RequestMapping("/bookings/")
 public class BookingController {
 	
 	@Autowired
 	private BookingService service;
 	
-	@PostMapping("/users/booking/makeBooking")
+	@PostMapping("customer/makeBooking")
 	public Booking makeBooking(@RequestBody Booking book) {
 		return service.makeBooking(book);
 	}
 	
 	
-	@DeleteMapping("/users/booking/cancelBooking/{bookingId}")
+	@DeleteMapping("customer/cancelBooking/{bookingId}")
 	public void deleteBooking(@PathVariable int bookingId) {
 		service.delBooking(bookingId);
 	}
